a0344be36e4b1d508aaa6a6cdf600b94f28959dd
Abraham Chavez <achave11@ucsc.edu>
2016-07-27 16:36:28 -0700

    Added 6 new tests. 1) test_pagging_token simply assures that the SearchVariantsResponse contains the page token when there is more variants to display. 2) test_offset_calculator assures that the 1-offset from the database and the query type are congruent. 3) test_get_variant_response assures that when we get a variant by id x, we obtain the variant whos id belongs to x. 4) test_brca_to_ga4gh_variantSets_status_code assures that the post is being done properly and that a useful response is returned. 5) test_brca_to_ga4gh_variantSets_metafieldNums needs to return the number of metadata fields within one SearchVariantSetsResponse. 6) test_variantSets_fields assures that the Response is served properly, with all the parameters that it should contain in the way that ga4gh Request is responded.

--- django/data/tests.py
+++ django/data/tests.py
@@ -11,7 +11,7 @@ from django.test.client import RequestFactory
 from brca import settings
 from data import test_data
 from data.models import Variant
-from data.views import index, autocomplete, index_num_2, brca_to_ga4gh, ErrorMessages
+from data.views import index, autocomplete, index_num_2, brca_to_ga4gh, ErrorMessages, get_offset, get_var_by_id, get_variantSet
 
 ##################### MY EDITS #########################
 
@@ -97,7 +97,7 @@ class VariantTestCase(TestCase):
 ############################# NEW TESTS START #####################################
     def test_ga4gh_variants_status_code(self):
         request0 =  self.factory.post(
-            "/data/ga4gh", json.dumps({"end": 51425158029, "referenceName": "chr17", "variantSetId": "brca1", "start": 51425158, "pageSize": 5 }), content_type="application/json")
+            "/data/ga4gh/variants/search", json.dumps({"end": 51425158029, "referenceName": "chr17", "variantSetId": "brca1", "start": 51425158, "pageSize": 5 }), content_type="application/json")
         response = index_num_2(request0)
         self.assertEqual(response.status_code, 200)
 
@@ -131,31 +131,82 @@ class VariantTestCase(TestCase):
 
     def test_validated_request(self):
         request = v_s.SearchVariantsRequest()
-        req = self.factory.post("/data/ga4gh", json.dumps(json_format._MessageToJsonObject(request, False)),
+        req = self.factory.post("/data/ga4gh/variants/search", json.dumps(json_format._MessageToJsonObject(request, False)),
                                 content_type="application/json")
         response = index_num_2(req)
         self.assertJSONEqual(response.content, ErrorMessages['VariantSetId'])
 
         request.variant_set_id = "Something not null"
-        req = self.factory.post("/data/ga4gh", json.dumps(json_format._MessageToJsonObject(request, False)),
+        req = self.factory.post("/data/ga4gh/variants/search", json.dumps(json_format._MessageToJsonObject(request, False)),
                                 content_type="application/json")
         response = index_num_2(req)
         self.assertJSONEqual(response.content, ErrorMessages['referenceName'])
 
         request.reference_name = "chr17"
-        req= self.factory.post("/data/ga4gh", json.dumps(json_format._MessageToJsonObject(request, False)),
+        req= self.factory.post("/data/ga4gh/variants/search", json.dumps(json_format._MessageToJsonObject(request, False)),
                                 content_type="application/json")
         response = index_num_2(req)
         self.assertJSONEqual(response.content, ErrorMessages['start'])
 
         request.start = 14589
-        req = self.factory.post("/data/ga4gh", json.dumps(json_format._MessageToJsonObject(request, False)),
+        req = self.factory.post("/data/ga4gh/variants/search", json.dumps(json_format._MessageToJsonObject(request, False)),
                                 content_type="application/json")
         response = index_num_2(req)
         self.assertJSONEqual(response.content, ErrorMessages['end'])
 
-    def test_pagging(self):
-        hibye = ""
+    def test_offset_calculator(self):
+        start = 12500
+        end = 13000
+        s1, e1 = get_offset(start, end) #Ending offset is the same, the query will be exclusive, and therefore query will satisfy type, that is inclusive.
+        self.assertEquals(s1, start+1)
+        self.assertEquals(e1, end+1)
+        opperlen = ['a','b']
+        s2, e2 = get_offset(start, end, opperlen)
+        self.assertEquals(s2, start - 1)
+        self.assertEquals(e2, s2+2)
+
+    def test_pagging_token(self):
+        request0 = self.factory.post(
+            "/data/ga4gh/variants/search", json.dumps(
+                {"end": 51425158029, "referenceName": "chr17", "variantSetId": "brca1", "start": 51425158,
+                 "pageSize": 5}), content_type="application/json")
+        response = index_num_2(request0)
+        self.assertEqual(json.loads(response.content)["nextPageToken"], "1" )
+
+        request0 = self.factory.post(
+            "/data/ga4gh/variants/search", json.dumps(
+                {"end": 51425158029, "referenceName": "chr17", "variantSetId": "brca1", "start": 51425158,
+                 "pageSize": 5, "pageToken": "3"}), content_type="application/json", )
+        response = index_num_2(request0)
+        self.assertEqual(json.loads(response.content)["nextPageToken"], "4")
+
+    def test_get_variant_response(self):
+        request = self.factory.get("/data/ga4gh/variants/hg37-1")
+        response = get_var_by_id(request, "hg37-1")
+        jsonresp = json.loads(response.content)
+        self.assertEqual(jsonresp["referenceName"], "chr13")
+        self.assertEqual(jsonresp["start"], "32923951")
+
+    def test_brca_to_ga4gh_variantSets_status_code(self):
+        request0 = self.factory.post(
+            "/data/ga4gh/variants/search", json.dumps({"datasetId": "hg37"}), content_type="application/json")
+        response = get_variantSet(request0)
+        self.assertEqual(response.status_code, 200)
+
+    def test_brca_to_ga4gh_variantSets_metafieldNums(self):
+        request0 = self.factory.post(
+            "/data/ga4gh/variants/search", json.dumps({"datasetId": "hg37"}), content_type="application/json")
+        response = get_variantSet(request0)
+        self.assertEqual(len(json.loads(response.content)["variantSets"][0]["metadata"]), 76)
+
+    def test_variantSets_fields(self):
+        request0 = self.factory.post(
+            "/data/ga4gh/variants/search", json.dumps({"datasetId": "hg37"}), content_type="application/json")
+        response = get_variantSet(request0)
+        Comp = json.loads(response.content)["variantSets"][0]
+        self.assertEqual(Comp["referenceSetId"], "Genomic_Coordinate_hg37")
+        self.assertEqual(Comp["datasetId"], "brca_exchange")
+        self.assertEqual(Comp["name"], "brca_exchange_hg37")
 
 if __name__ == '__main__':
     unittest.main()
