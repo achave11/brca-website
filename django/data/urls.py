0bec861b654485f5b1b936da22ea5af89dee9409
Abraham Chavez <achave11@ucsc.edu>
2016-07-27 16:38:47 -0700

    2 URLs where added so that (get-variant by id) and (post-variantsets) requests are received by the front-end

--- django/data/urls.py
+++ django/data/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     url(r'^$', views.index, name="index"),
     url(r'^suggestions/$', views.autocomplete),
-    url(r'^ga4gh/$', views.index_num_2, name='index_num_2')
+    url(r'^ga4gh/variants/search$', views.index_num_2, name='index_num_2'),
+    url(r'^ga4gh/variants/(?P<variant_id>.+)$', views.get_var_by_id, name = 'get_var_by_id'),
+    url(r'^ga4gh/variantsets/search', views.get_variantSet, name='get_variantSet')
 ]
